9438e86a1dace54d8537fe79b862d5bb10c9a23a
Martin Algesten <martin@algesten.se>
2019-01-23 18:12:26 +0530

    build

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,11 @@
+export declare type TestFunction = {
+    (name: string, testfn: () => any | Promise<any>): void;
+    <S>(name: string, before: () => S | Promise<S>, testfn: (s: S) => any | Promise<any>, after?: (s: S) => any | Promise<any>): void;
+    <S>(name: string, def: {
+        before?: () => S | Promise<S>;
+        testfn: (s: S) => any | Promise<any>;
+        after?: (s: S) => any | Promise<any>;
+    }): void;
+};
 /** Declare a test function. */
-export declare const test: (name: string, fn: () => any) => void;
+export declare const test: TestFunction;
