4ef3437d883663c7c37890d2e65ac008ce8d1672
Johan Brook <johan@lookback.io>
2019-02-04 22:54:47 +0100

    Add pretty stack info to TAP reporter

--- src/reporters/tap-reporter.ts
+++ src/reporters/tap-reporter.ts
@@ -1,12 +1,64 @@
+import { basename } from 'path';
 import { Reporter } from ".";
+import { SerializedError } from "../lib/serialize-error";
+import { isPlainObject } from "../lib/is-plain-object";
+
+const getStackInfo = (stack: string): { line?: string, column?: string, file: string } | string => {
+    const stackArr = stack.split('\n');
+    const stackLine = stackArr[0].includes('ERR_ASSERTION')  ? stackArr[1] : stackArr[0];
+    const match = stackLine.match(/\((\/.*)\)/);
+
+    if (!match) {
+        return stackLine;
+    }
+
+    const base = basename(match[1]);
+    const [, file, line, column]: string[] = base.match(/(.*):(\d+):(\d+)/) || [];
+
+    return { file, line, column };
+};
+
+const formatError = (error: SerializedError): object => {
+    const stack = error.stack ? getStackInfo(error.stack) : undefined;
+
+    return {
+        error: error.name,
+        message: error.message,
+        at: stack ? stack : undefined,
+    };
+};
+
+const toYAML = (obj: object, indent = 0): string =>
+    Object.entries(obj)
+        .map(
+            ([key, val]) =>
+                `${' '.repeat(indent)}${key}: ${
+                isPlainObject(val)
+                    ? `\n${toYAML(val, indent + 3)}`
+                    : String(val)
+                }`
+        )
+        .join(`\n`);
+
+const outputDiagnostics = (obj: any): string =>
+    `
+   ---
+${toYAML(obj, 3)}
+   ...
+`;
+
+const logSuccess = (title: string, index: number) =>
+    `ok ${index + 1} - ${title}`;
+
+const logFailure = (title:  string, index: number, error?: SerializedError) =>
+    `not ok ${index + 1} - ${title}` + (error ? outputDiagnostics(formatError(error)) : '');
 
 const TAPReporter: Reporter = {
     startRun: ({ numFiles, total }) =>
         `TAP version 13\n1..${total}`,
 
-    test: ({ title, passed, index }) => {
-        return `${passed ? '': 'not '}ok ${index + 1} ${title}`;
-    },
+    test: ({ title, passed, index, error }) =>
+        passed ? logSuccess(title, index) : logFailure(title, index, error),
 
     finishRun: () => {
         return '';
